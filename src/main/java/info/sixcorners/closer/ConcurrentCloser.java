ce12349ab48608e721d1cbbf82f64f0159a11761
sixcorners <sixcorners@gmail.com>
2025-01-09 15:34:15 -0500

    Add ConcurrentCloser

--- src/main/java/info/sixcorners/closer/ConcurrentCloser.java
+++ src/main/java/info/sixcorners/closer/ConcurrentCloser.java
@@ -1,35 +1,55 @@
 package info.sixcorners.closer;
 
 import java.io.Closeable;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 import java.lang.ref.Cleaner;
-import java.util.stream.Stream;
 import javax.imageio.stream.ImageInputStream;
-import lombok.Lombok;
 import lombok.val;
 
 /**
- * Collects and closes resources in the reverse order
+ * Thread-safe version of {@link Closer}
  *
- * <p>Inspired by {@link Stream#close}. Not thread-safe. Also contains static utility methods for
- * closing resources that are thread-safe. If you have Java 9+ and want a thread-safe version try
- * {@link ConcurrentCloser}.
+ * <p>Only works in Java 9+.
+ *
+ * <p>One way to use this could be to put your stuff into a normal {@link Closer} then add it to
+ * this instead of calling {@link #onClose} repeatedly. That does change the order slightly though.
  */
-public final class Closer implements Closeable, Runnable {
-  private Handle head;
+@SuppressWarnings({"FieldMayBeFinal", "unused"})
+public final class ConcurrentCloser implements Closeable, Runnable {
+  private static final Handle CLOSED = new Handle(null);
+  private static final VarHandle HEAD;
 
-  /** Registers an {@link AutoCloseable} to be closed in reverse order */
-  public Handle onClose(AutoCloseable closeable) {
-    val newNode = new Handle(closeable);
-    newNode.next = head;
-    return head = newNode;
+  static {
+    val mh = MethodHandles.lookup();
+    try {
+      HEAD = mh.findVarHandle(ConcurrentCloser.class, "head", Handle.class);
+    } catch (ReflectiveOperationException e) {
+      throw new IllegalStateException("findVarHandle", e);
+    }
   }
 
-  /** Closes registered {@link AutoCloseable}s in reverse order */
-  @Override
-  public void close() {
+  private Handle head;
+
+  /**
+   * Registers an {@link AutoCloseable} to be closed in reverse order
+   *
+   * <p>Does not work while this {@link ConcurrentCloser} is closed.
+   */
+  public Handle onClose(AutoCloseable closeable) {
+    val newNode = new Handle(closeable);
+    Handle oldNode = head;
+    do {
+      if (oldNode == CLOSED) throw new IllegalStateException("closed");
+      newNode.next = oldNode;
+      oldNode = (Handle) HEAD.compareAndExchangeRelease(this, oldNode, newNode);
+    } while (newNode.next != oldNode);
+    return newNode;
+  }
+
+  private void close(Handle newHead) {
     Throwable t = null;
-    Handle handle = head;
-    head = null;
+    Handle handle = (Handle) HEAD.getAndSetAcquire(this, newHead);
     while (handle != null) {
       try {
         handle.justClose();
@@ -44,7 +64,21 @@ public final class Closer implements Closeable, Runnable {
       handle = handle.next;
       oldHandle.next = null; // things might hold onto the Handle and prevent gc
     }
-    if (t != null) throw sneakyThrow(t);
+    if (t != null) throw Closer.sneakyThrow(t);
+  }
+
+  /** Closes registered {@link AutoCloseable}s in reverse order */
+  @Override
+  public void close() {
+    close(null);
+  }
+
+  /**
+   * Closes registered {@link AutoCloseable}s in reverse order and prevents successful calls to
+   * {@link #onClose}
+   */
+  public void closeForGood() {
+    close(CLOSED);
   }
 
   /** Calls {@link #close} */
@@ -53,10 +87,20 @@ public final class Closer implements Closeable, Runnable {
     close();
   }
 
-  /** Handle to one {@link AutoCloseable} registered in a {@link Closer} */
+  /** Handle to one {@link AutoCloseable} registered in a {@link ConcurrentCloser} */
   public static final class Handle implements Closeable, Runnable {
+    private static final VarHandle CLOSEABLE;
     private Handle next;
     private AutoCloseable closeable;
+
+    static {
+      val mh = MethodHandles.lookup();
+      try {
+        CLOSEABLE = mh.findVarHandle(Handle.class, "closeable", AutoCloseable.class);
+      } catch (ReflectiveOperationException e) {
+        throw new IllegalStateException("findVarHandle", e);
+      }
+    }
 
     /**
      * Can be used as a wrapper that only performs a close once
@@ -70,11 +114,8 @@ public final class Closer implements Closeable, Runnable {
     }
 
     private void justClose() {
-      val closeable = this.closeable;
-      if (closeable != null) {
-        this.closeable = null;
-        Closer.close(closeable);
-      }
+      val closeable = (AutoCloseable) CLOSEABLE.getAndSetRelease(this, null);
+      if (closeable != null) Closer.close(closeable);
     }
 
     /**
@@ -129,49 +170,5 @@ public final class Closer implements Closeable, Runnable {
     int i = 0;
     for (Handle handle = head; handle != null; handle = handle.next) i++;
     return i;
-  }
-
-  @SuppressWarnings("unchecked")
-  private static <T extends Throwable> T sneakyThrow0(Throwable t) throws T {
-    throw (T) t;
-  }
-
-  /**
-   * Throws as an unchecked exception.
-   *
-   * <p>Works like how Kotlin throws checked exceptions.
-   *
-   * @param t the throwable to rethrow
-   * @return never returns; always throws
-   * @see Lombok#sneakyThrow
-   */
-  public static RuntimeException sneakyThrow(Throwable t) {
-    return sneakyThrow0(t);
-  }
-
-  /** Closes with throwing */
-  public static void close(AutoCloseable closeable) {
-    try {
-      closeable.close();
-    } catch (Exception e) {
-      throw sneakyThrow(e);
-    }
-  }
-
-  /** Closes without throwing */
-  public static void closeQuietly(AutoCloseable closeable) {
-    try {
-      closeable.close();
-    } catch (Throwable ignored) {
-    }
-  }
-
-  /**
-   * Converts an {@link AutoCloseable} to a {@link Runnable}
-   *
-   * <p>Useful for {@link Cleaner#register} and {@link Stream#onClose}.
-   */
-  public static Runnable toRunnable(AutoCloseable closeable) {
-    return () -> close(closeable);
   }
 }
